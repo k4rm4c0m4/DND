f1f9c27da2e724fc1f229d244fba3eb3ebdeb12b
Speedwagon Foundation <simon@busch.pro>
2021-12-22 08:36:55 +0000

    - added license headers - added a function to "roll a dice" with x faces - added function to get the value of a skill roll - updated main to demonstrate skill roll function -updated readme

--- src/dnd/NonPlayerCharacter.java
+++ src/dnd/NonPlayerCharacter.java
@@ -1,4 +1,8 @@
-package dnd;
+/**
+ * (c) 2021 Simon Busch, Maurice Bonke & Mikail Yangin under the GNU General Purpose License v3.0
+ */
+
+ package dnd;
 
 public class NonPlayerCharacter extends Character
 {
